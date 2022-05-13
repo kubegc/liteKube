d66e14cc21dd3c2316d6b73b6bd5cbd3ab8c9f43
oneflyingfish <a_flying_fish@outlook.com>
2022-05-13 17:24:30 +0800

    compile by docker

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
-version=0.1.0
+ProjectPath=/LiteKube
+Version=0.1.0
+GitBranch=$(git rev-parse --abbrev-ref HEAD)
+GitVersion=$(git version)
+GitCommit=$(git rev-parse HEAD)
+BuildDate=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
+LeaderFold=/LiteKube/build/outputs/leader
+WorkerFold=/LiteKube/build/outputs/worker
 
-VersionTags="
-    -X \"github.com/litekube/LiteKube/pkg/version.Litekube=$version\"
-    -X \"github.com/litekube/LiteKube/pkg/version.GitBranch=`git branch`\"
-    -X \"github.com/litekube/LiteKube/pkg/version.GitVersion=`git version`\" 
-    -X \"github.com/litekube/LiteKube/pkg/version.GitCommit=`git rev-parse HEAD`\"
-    -X \"github.com/litekube/LiteKube/pkg/version.BuildDate=`date -u +'%Y-%m-%dT%H:%M:%SZ'`\"
+VersionTags="\
+    -X \"github.com/litekube/LiteKube/pkg/version.Litekube=$Version\" \
+    -X \"github.com/litekube/LiteKube/pkg/version.GitBranch=$GitBranch\" \
+    -X \"github.com/litekube/LiteKube/pkg/version.GitVersion=$GitVersion\" \
+    -X \"github.com/litekube/LiteKube/pkg/version.GitCommit=$GitCommit\" \
+    -X \"github.com/litekube/LiteKube/pkg/version.BuildDate=$BuildDate\" \
 "
 
-mkdir -p /Litekube/build/outputs/leader
-cd /Litekube/cmd/leader
-go build -ldflags "$VersionTags" -o leader-${uname}-${arch}-$version .
-mv leader-linux-amd64-$version /Litekube/build/outputs/leader/
-env CGO_ENABLED=1 GOOS=linux GOARCH=arm CC=arm-linux-gnueabihf-gcc go build -ldflags "$VersionTags" -o leader-Linux-arm-$version .
-mv leader-linux-arm-$version /Litekube/build/outputs/leader/
 
-mkdir -p /Litekube/build/outputs/worker
-cd /Litekube/cmd/worker
-go build -ldflags "$VersionTags" -o worker-${uname}-${arch}-$version .
-mv worker-linux-amd64-$version /Litekube/build/outputs/worker/
-env CGO_ENABLED=1 GOOS=linux GOARCH=arm CC=arm-linux-gnueabihf-gcc go build -ldflags "$VersionTags" -o worker-Linux-arm-$version .
-mv worker-linux-arm-$version /Litekube/build/outputs/worker/
+mkdir -p $LeaderFold
+cd $ProjectPath/cmd/leader
+
+Tag=leader-$(uname)-$(arch)-$Version
+echo "build $Tag"
+go build -ldflags "$VersionTags" -o $Tag . && mv $Tag $LeaderFold
+
+Tag=leader-Linux-arm-$Version
+echo "build $Tag"
+env CGO_ENABLED=1 GOOS=linux GOARCH=arm CC=arm-linux-gnueabihf-gcc go build -ldflags "$VersionTags" -o $Tag . && mv $Tag $LeaderFold
+
+
+mkdir -p $WorkerFold
+cd $ProjectPath/cmd/worker
+
+Tag=worker-$(uname)-$(arch)-$Version
+echo "build $Tag"
+go build -ldflags "$VersionTags" -o $Tag . && mv $Tag $WorkerFold
+Tag=worker-Linux-arm-$Version
+echo "build $Tag"
+env CGO_ENABLED=1 GOOS=linux GOARCH=arm CC=arm-linux-gnueabihf-gcc go build -ldflags "$VersionTags" -o $Tag . && mv $Tag $WorkerFold
